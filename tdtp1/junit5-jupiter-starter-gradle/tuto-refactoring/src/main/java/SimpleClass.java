631b2226ec4392999803a0dfb7b90584e0f67d18
Evan SPICKA <evanspicka@gmail.com>
2024-02-06 14:46:55 +0100

    Rennomage

---
+++ tdtp1/junit5-jupiter-starter-gradle/tuto-refactoring/src/main/java/SimpleClass.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        new SimpleClass().someAdditionMethod(1, 2);
+    }
+    public int someAdditionMethod(int a, int b) {
+        return a + b;
+    }
+}
+
+

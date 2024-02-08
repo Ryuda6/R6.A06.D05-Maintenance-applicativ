643c2615a67a82c50b1b68281bbeb73f18fad5c0
Evan SPICKA <evanspicka@gmail.com>
2024-02-08 10:09:42 +0100

    Abaisser

--- tdtp1/junit5-jupiter-starter-gradle/tuto-refactoring/src/main/java/Base.java
+++ tdtp1/junit5-jupiter-starter-gradle/tuto-refactoring/src/main/java/Base.java
@@ -1,5 +1,2 @@
 public class Base {
-    protected int doubleValue(int number) {
-        return number + number;
-    }
 }
--- tdtp1/junit5-jupiter-starter-gradle/tuto-refactoring/src/main/java/Derived.java
+++ tdtp1/junit5-jupiter-starter-gradle/tuto-refactoring/src/main/java/Derived.java
@@ -3,4 +3,8 @@ public class Derived extends Base {
         Derived subject = new Derived();
         System.out.println("Doubling 21. Result: " + subject.doubleValue(21));
     }
+
+    protected int doubleValue(int number) {
+        return number + number;
+    }
 }
